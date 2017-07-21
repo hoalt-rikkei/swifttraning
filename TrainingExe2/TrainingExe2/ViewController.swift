86b76714df716f04a5aa79171a08e1aee669cdcb
hoalt <boycntt92@gmail.com>
2017-07-21 08:41:47 +0700

    create TrainingExe2

---
+++ TrainingExe2/TrainingExe2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TrainingExe2
+//
+//  Created by ios developer on 7/21/17.
+//  Copyright © 2017 leeboy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
