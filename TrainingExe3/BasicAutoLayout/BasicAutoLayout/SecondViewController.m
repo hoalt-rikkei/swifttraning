504ce561d3f633e0465ce15061a6bb9f057952e3
hoalt <boycntt92@gmail.com>
2017-07-27 14:15:26 +0700

    - add constraint using code

--- TrainingExe3/BasicAutoLayout/BasicAutoLayout/SecondViewController.m
+++ TrainingExe3/BasicAutoLayout/BasicAutoLayout/SecondViewController.m
@@ -39,14 +39,14 @@
     self.redView = [[UIView alloc] initWithFrame:CGRectMake(125, 400, 70, 70)];
     self.redView.backgroundColor = [UIColor redColor];
     [self.view addSubview:self.redView];
-    
-    
 }
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     self.title = @"Second";
+    
+    [self addViewConstraint];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -54,20 +54,189 @@
     // Dispose of any resources that can be recreated.
 }
 
-- (void)updateViewConstraints {
-    [self.view addConstraints:[NSLayoutConstraint
-                               constraintsWithVisualFormat:@"V:|-[grayView(>=200)]-|"
-                               options:NSLayoutFormatDirectionLeadingToTrailing
-                               metrics:nil
-                               views:NSDictionaryOfVariableBindings(self.grayView)]];
+- (void)addViewConstraint {
+    UIView *parent= self.view;
     
-    [self.view addConstraints:[NSLayoutConstraint
-                               constraintsWithVisualFormat:@"H:[grayView(==200)]-|"
-                               options:NSLayoutFormatDirectionLeadingToTrailing
-                               metrics:nil
-                               views:NSDictionaryOfVariableBindings(self.grayView)]];
-}
+    _grayView.translatesAutoresizingMaskIntoConstraints = NO;
+    _blueView.translatesAutoresizingMaskIntoConstraints = NO;
+    _textLabel.translatesAutoresizingMaskIntoConstraints = NO;
+    _redView.translatesAutoresizingMaskIntoConstraints = NO;
+    self.edgesForExtendedLayout = UIRectEdgeNone;
+    
+    //grayView constraint
+    NSLayoutConstraint *grayLeft =[NSLayoutConstraint
+                               constraintWithItem:_grayView
+                               attribute:NSLayoutAttributeLeftMargin
+                               relatedBy:NSLayoutRelationEqual
+                               toItem:parent
+                               attribute:NSLayoutAttributeLeftMargin
+                               multiplier:1.0f
+                               constant:8];
 
+    NSLayoutConstraint *grayRight = [NSLayoutConstraint
+                                 constraintWithItem:_grayView
+                                 attribute:NSLayoutAttributeRightMargin
+                                 relatedBy:NSLayoutRelationEqual
+                                 toItem:parent
+                                 attribute:NSLayoutAttributeRightMargin
+                                 multiplier:1.0f
+                                 constant:-8];
+    
+    NSLayoutConstraint *grayTop =[NSLayoutConstraint
+                              constraintWithItem:_grayView
+                              attribute:NSLayoutAttributeTopMargin
+                              relatedBy:NSLayoutRelationEqual
+                              toItem:parent
+                              attribute:NSLayoutAttributeTopMargin
+                              multiplier:1.0f
+                              constant:22];
+    
+    NSLayoutConstraint *grayHeight = [NSLayoutConstraint
+                                  constraintWithItem:_grayView
+                                  attribute:NSLayoutAttributeHeight
+                                  relatedBy:NSLayoutRelationEqual
+                                  toItem:nil
+                                  attribute:NSLayoutAttributeNotAnAttribute
+                                  multiplier:0
+                                  constant:129];
+    
+    //blueView constraint
+    NSLayoutConstraint *blueRight = [NSLayoutConstraint
+                                     constraintWithItem:_blueView
+                                     attribute:NSLayoutAttributeRightMargin
+                                     relatedBy:NSLayoutRelationEqual
+                                     toItem:parent
+                                     attribute:NSLayoutAttributeRightMargin
+                                     multiplier:1.0f
+                                     constant:-8];
+    
+    NSLayoutConstraint *blueTop =[NSLayoutConstraint
+                                  constraintWithItem:_blueView
+                                  attribute:NSLayoutAttributeTopMargin
+                                  relatedBy:NSLayoutRelationEqual
+                                  toItem:parent
+                                  attribute:NSLayoutAttributeTopMargin
+                                  multiplier:1.0f
+                                  constant:160];
+    
+    NSLayoutConstraint *blueWidth = [NSLayoutConstraint
+                                      constraintWithItem:_blueView
+                                      attribute:NSLayoutAttributeWidth
+                                      relatedBy:NSLayoutRelationEqual
+                                      toItem:nil
+                                      attribute:NSLayoutAttributeNotAnAttribute
+                                      multiplier:0
+                                      constant:73];
+    
+    NSLayoutConstraint *blueHeight = [NSLayoutConstraint
+                                      constraintWithItem:_blueView
+                                      attribute:NSLayoutAttributeHeight
+                                      relatedBy:NSLayoutRelationEqual
+                                      toItem:nil
+                                      attribute:NSLayoutAttributeNotAnAttribute
+                                      multiplier:0
+                                      constant:67];
+    
+    //textLabel constraint
+    NSLayoutConstraint *textRight = [NSLayoutConstraint
+                                     constraintWithItem:_textLabel
+                                     attribute:NSLayoutAttributeRightMargin
+                                     relatedBy:NSLayoutRelationEqual
+                                     toItem:parent
+                                     attribute:NSLayoutAttributeRightMargin
+                                     multiplier:1.0f
+                                     constant:-100];
+    
+    NSLayoutConstraint *textTop =[NSLayoutConstraint
+                                  constraintWithItem:_textLabel
+                                  attribute:NSLayoutAttributeTopMargin
+                                  relatedBy:NSLayoutRelationEqual
+                                  toItem:parent
+                                  attribute:NSLayoutAttributeTopMargin
+                                  multiplier:1.0f
+                                  constant:160];
+    
+    NSLayoutConstraint *textLeft = [NSLayoutConstraint
+                                     constraintWithItem:_textLabel
+                                     attribute:NSLayoutAttributeLeftMargin
+                                     relatedBy:NSLayoutRelationEqual
+                                     toItem:parent
+                                     attribute:NSLayoutAttributeLeftMargin
+                                     multiplier:1.0f
+                                     constant:8];
+    
+    NSLayoutConstraint *textHeight = [NSLayoutConstraint
+                                      constraintWithItem:_textLabel
+                                      attribute:NSLayoutAttributeHeight
+                                      relatedBy:NSLayoutRelationEqual
+                                      toItem:nil
+                                      attribute:NSLayoutAttributeNotAnAttribute
+                                      multiplier:0
+                                      constant:67];
+    
+    //redView constraint
+    NSLayoutConstraint *redBottom = [NSLayoutConstraint
+                                     constraintWithItem:_redView
+                                     attribute:NSLayoutAttributeBottomMargin
+                                     relatedBy:NSLayoutRelationEqual
+                                     toItem:parent
+                                     attribute:NSLayoutAttributeBottomMargin
+                                     multiplier:1.0f
+                                     constant:-15];
+    
+    NSLayoutConstraint *redRight =[NSLayoutConstraint
+                                  constraintWithItem:_redView
+                                  attribute:NSLayoutAttributeCenterX
+                                  relatedBy:NSLayoutRelationEqual
+                                  toItem:parent
+                                  attribute:NSLayoutAttributeCenterX
+                                  multiplier:1.0f
+                                  constant:0];
+    
+    NSLayoutConstraint *redWidth = [NSLayoutConstraint
+                                     constraintWithItem:_redView
+                                     attribute:NSLayoutAttributeWidth
+                                     relatedBy:NSLayoutRelationEqual
+                                     toItem:nil
+                                     attribute:NSLayoutAttributeNotAnAttribute
+                                     multiplier:0
+                                     constant:71];
+    
+    NSLayoutConstraint *redHeight = [NSLayoutConstraint
+                                      constraintWithItem:_redView
+                                      attribute:NSLayoutAttributeHeight
+                                      relatedBy:NSLayoutRelationEqual
+                                      toItem:nil
+                                      attribute:NSLayoutAttributeNotAnAttribute
+                                      multiplier:0
+                                      constant:69];
+    
+    //Add constraints to the Parent
+    [parent addConstraint:grayLeft];
+    [parent addConstraint:grayTop];
+    [parent addConstraint:grayRight];
+    
+    [parent addConstraint:blueTop];
+    [parent addConstraint:blueRight];
+    
+    [parent addConstraint:textRight];
+    [parent addConstraint:textTop];
+    [parent addConstraint:textLeft];
+    
+    [parent addConstraint:redBottom];
+    [parent addConstraint:redRight];
+    
+    //Add height constraint to the subview, as subview owns it.
+    [_grayView addConstraint:grayHeight];
+    
+    [_blueView addConstraint:blueWidth];
+    [_blueView addConstraint:blueHeight];
+    
+    [_textLabel addConstraint:textHeight];
+    
+    [_redView addConstraint:redWidth];
+    [_redView addConstraint:redHeight];
+}
 /*
 #pragma mark - Navigation
 
